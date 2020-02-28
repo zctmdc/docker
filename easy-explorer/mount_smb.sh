86563c93d44a088ecbae5183400c3df8966167cf
ZCTMDC <zctmdc@outlook.com>
2020-02-28 14:28:27 +0800

    fix mount bug

--- easy-explorer/mount_smb.sh
+++ easy-explorer/mount_smb.sh
@@ -2,7 +2,7 @@
 set -x
 
 mkdir -p /mnt/share/smb/onecloud/1t
-mount.cifs //192.168.123.111/cb7a /mnt/share/smb/onecloud/1t -o username=$SMB_USERNAME,password=$SMB_PASSWORD
+mount.cifs //192.168.60.111/cb7a /mnt/share/smb/onecloud/1t -o username=$SMB_USERNAME,password=$SMB_PASSWORD
 
 mkdir -p /mnt/share/smb/onecloud/4t
-mount.cifs //192.168.123.111/f6d3 /mnt/share/smb/onecloud/4t -o username=$SMB_USERNAME,password=$SMB_PASSWORD
+mount.cifs //192.168.60.111/f6d3 /mnt/share/smb/onecloud/4t -o username=$SMB_USERNAME,password=$SMB_PASSWORD
