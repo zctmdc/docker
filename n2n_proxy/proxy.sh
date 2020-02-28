3ad97a9e63ed53deb7660e8915e298ccaa99d4c8
ZCTMDC <zctmdc@outlook.com>
2020-02-28 11:03:38 +0800

    修复添加防火墙 代码bug
    修复添加防火墙 代码bug

--- n2n_proxy/proxy.sh
+++ n2n_proxy/proxy.sh
@@ -12,13 +12,15 @@ if [[ "${N2N_NAT}" == "TRUE" ]] ;then
   wan_eth="eth0"
   lan_ip=`ifconfig $lan_eth| grep "inet addr:" | awk '{print $2}' | cut -c 6-`
   lan_gateway=`ifconfig $lan_eth |sed -n '/inet addr/s/^[^:]*:\(\([0-9]\{1,3\}\.\)\{3\}\).*/\1/p'`1
+  lan_subnet=`ifconfig $lan_eth |sed -n '/inet addr/s/^[^:]*:\(\([0-9]\{1,3\}\.\)\{3\}\).*/\1/p'`0/24
   wan_ip=`ifconfig $wan_eth| grep "inet addr:" | awk '{print $2}' | cut -c 6-`
   wan_gateway=`ifconfig $wan_eth |sed -n '/inet addr/s/^[^:]*:\(\([0-9]\{1,3\}\.\)\{3\}\).*/\1/p'`1
+  wan_subnet=`ifconfig $wan_eth |sed -n '/inet addr/s/^[^:]*:\(\([0-9]\{1,3\}\.\)\{3\}\).*/\1/p'`0/24
 
   sysctl net.ipv4.ip_forward=1
   iptables -A INPUT -i $lan_eth -j ACCEPT
   iptables -A FORWARD -i $wan_eth -j ACCEPT
-  iptables -t nat -A POSTROUTING -s $lan_gateway/24 -o $wan_eth -j MASQUERADE
+  iptables -t nat -A POSTROUTING -s $lan_subnet -o $wan_eth -j MASQUERADE
 fi
 route -n
 if [[ "${N2N_PROXY}" == "TRUE" ]] ;then
