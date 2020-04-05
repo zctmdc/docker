b55cb6cf0aebdf87b7005b43055d09132abf90ca
ZCTMDC <zctmdc@outlook.com>
2020-04-05 22:59:43 +0800

    删除多余文件

--- easy-explorer-smb/mount_smb.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -x
-touch /var/log/mount_smb.log
-mkdir -p /mnt/share/smb/onecloud/1t
-mount.cifs //192.168.60.111/cb7a /mnt/share/smb/onecloud/1t -o username=$SMB_USERNAME,password=$SMB_PASSWORD
-
-mkdir -p /mnt/share/smb/onecloud/4t
-mount.cifs //192.168.60.111/f6d3 /mnt/share/smb/onecloud/4t -o username=$SMB_USERNAME,password=$SMB_PASSWORD
--- easy-explorer-smb/run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -x
-nohup /usr/local/sbin/mount_smb.sh >>/var/log/run.log 2>&1 &
-nohup /usr/local/sbin/easy-explorer.sh >>/var/log/run.log 2>&1 &
-tail -f -n 20 /var/log/run.log
