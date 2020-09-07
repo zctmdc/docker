48e5164624d24a2c4ad94384c557faaf60f92942
ZCTMDC <zctmdc@outlook.com>
2020-09-07 09:49:10 +0800

    update check_uri

--- frp/healthcheck_frpc.sh
+++ frp/healthcheck_frpc.sh
@@ -1,4 +1,4 @@
-check_uri="http://${user}-admin.${subdomain_host}:${vhost_http_port}"
+check_uri="http://${user}-admin.${subdomain_host}:${vhost_http_port}/api/status"
 frpc_status_code="$(curl -H -I -m 2 -o /dev/null -s -w %{http_code} ${check_uri})"
 echo "STATUS : FRPC - ${frpc_status_code}"
 if [[ ${frpc_status_code} == 404 ]]; then
--- frp/healthcheck_frps.sh
+++ frp/healthcheck_frps.sh
@@ -1,4 +1,4 @@
-check_uri="http://${server_addr}:${dashboard_port}"
+check_uri="http://${server_addr}:${dashboard_port}/api/serverinfo"
 frps_status_code="$(curl -H -I -m 2 -o /dev/null -s -w %{http_code} ${check_uri})"
 echo "STATUS : FRPS - ${frps_status_code}"
 if [[ ${frps_status_code} == 404 ]]; then
