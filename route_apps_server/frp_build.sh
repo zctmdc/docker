9f8592786363e42fd49863ff7e211f6ee29290a0
zctmdc <zctmdc@outlook.com>
2020-10-04 01:16:10 +0800

    fix bugs

--- route_apps_server/frp_build.sh
+++ route_apps_server/frp_build.sh
@@ -1,13 +1,18 @@
-frp_version=$(./bin/frps --version)
-echo "frp version: $frp_version"
 /tmp/frp/
 git clone https://github.com/fatedier/frp.git /tmp/frp/
 cd /tmp/frp/
 ./package.sh
 
+frp_version=$(/tmp/frp/bin/frps --version)
+echo "frp version: $frp_version"
 
 cd /tmp/frp/release/packages/
 
+replaseKV='
+el-le
+amd64-x64
+386-x86
+'
 os_all='linux windows darwin freebsd'
 arch_all='386 amd64 arm arm64 mips64 mips64le mips mipsle'
 
@@ -15,15 +20,29 @@ for os in $os_all; do
     for arch in $arch_all; do
         frp_dir_name="frp_${frp_version}_${os}_${arch}"
         for frp_action in frpc frps; do
-            if [ "x${os}" = x"windows" ]; then
-                zip -rq ${frp_dir_name}.zip
-                cp -f ${frp_dir_name}/${frp_action}.exe $FRP_OPT_DIR/${frp_action}_${os}_${arch}.exe
-            else
-                tar -zxf ${frp_dir_name}.tar.gz
-                cp -f ${frp_dir_name}/${frp_action} $FRP_OPT_DIR/${frp_action}_${os}_${arch}
-            fi
+            for line_rep in ${replaseKV}; do
+                line_rep_k="${line_rep%-*}"
+                line_rep_v="${line_rep#*-}"
+                if [ "x${os}" = x"windows" ]; then
+                    zip -rq ${frp_dir_name}.zip
+                    cp -f ${frp_dir_name}/${frp_action}.exe $FRP_OPT_DIR/${frp_action}_${os}_${arch}.exe
+                    if [[ "${arch}" == *"${line_rep_k}" ]]; then
+                        cp -f ${frp_dir_name}/${frp_action}.exe $FRP_OPT_DIR/${frp_action}_${os}_${arch%%${line_rep_k}}${line_rep_v}.exe
+                    fi
+                    if [[ "${arch}" == *"${line_rep_v}" ]]; then
+                        cp -f ${frp_dir_name}/${frp_action}.exe $FRP_OPT_DIR/${frp_action}_${os}_${arch%%${line_rep_v}}${line_rep_k}.exe
+                    fi
+                else
+                    tar -zxf ${frp_dir_name}.tar.gz
+                    cp -f ${frp_dir_name}/${frp_action} $FRP_OPT_DIR/${frp_action}_${os}_${arch}
+                    if [[ "${arch}" == *"${line_rep_k}" ]]; then
+                        cp -f ${frp_dir_name}/${frp_action} $FRP_OPT_DIR/${frp_action}_${os}_${arch%%${line_rep_k}}${line_rep_v}
+                    fi
+                    if [[ "${arch}" == *"${line_rep_v}" ]]; then
+                        cp -f ${frp_dir_name}/${frp_action} $FRP_OPT_DIR/${frp_action}_${os}_${arch%%${line_rep_v}}${line_rep_k}
+                    fi
+                fi
+            done
         done
     done
 done
-
-cd -
