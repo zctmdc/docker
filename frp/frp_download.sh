13abeafaac0d23fc284f6e55f0b8b1908ff8acb5
zctmdc <zctmdc@outlook.com>
2021-06-27 22:44:50 +0800

    upadte n2n : 更改下载源为 lucktu/n2n

--- frp/frp_download.sh
+++ frp/frp_download.sh
@@ -1,38 +1,40 @@
 #!/bin/bash
-source init_logger.sh
-source init_kernel_name_and_machine_name.sh
+. init_logger.sh
+. init_kernel_name_and_machine_name.sh
 
 if [[ -z ${KERNEL} ]]; then
-  myos=""
-  zctmdc_sel_os
-  case $myos in
-  linux | freebsd | darwin | windows)
-    LOG_INFO "受支持的系统 - $myos"
+  case ${myos} in
+  linux | freebsd | windows)
+    LOG_INFO "受支持的系统 - ${myos}"
+    KERNEL=${myos}
+    ;;
+  macosx)
+    LOG_INFO "受支持的系统 - ${myos}"
+    KERNEL="darwin"
     ;;
   *)
-    LOG_ERROR "不支持的系统 - $myos"
+    LOG_ERROR "不支持的系统 - ${myos}"
     exit 1
     ;;
   esac
-  KERNEL="$myos"
+
 fi
 if [[ -z ${MACHINE} ]]; then
-  mycpu=""
-  zctmdc_sel_cpu
-  case $mycpu in
+  case ${mycpu} in
   i386)
-    mycpu=386
-    LOG_INFO "受支持的CPU架构类型 - $mycpu"
+    MACHINE="386"
+    LOG_INFO "受支持的CPU架构类型 - ${mycpu}"
     ;;
   arm | arm64 | mips | mips64 | mips64el | mipsel | amd64)
-    LOG_INFO "受支持的CPU架构类型 - $mycpu"
+    LOG_INFO "受支持的CPU架构类型 - ${mycpu}"
+    MACHINE=${mycpu}
     ;;
   *)
-    LOG_ERROR "不支持的CPU架构类型 - $mycpu"
+    LOG_ERROR "不支持的CPU架构类型 - ${mycpu}"
     exit 1
     ;;
   esac
-  MACHINE=$mycpu
+
 fi
 
 if [[ -z ${FRP_VERSION} ]]; then
@@ -42,13 +44,13 @@ if [[ -z ${FRP_VERSION} ]]; then
       grep -oP "(\d+\.){2}\d+" |
       head -n 1
   )
-  LOG_INFO "FRP_VERSION : GITHUB - $FRP_VERSION"
+  LOG_INFO "FRP_VERSION : GITHUB - ${FRP_VERSION}"
 fi
 
 if [[ -z ${FRP_VERSION} ]]; then
   LOG_INFO "正在从七牛云获取版本"
   FRP_VERSION=$(curl -sS http://rt.qiniu.zctmdc.cn/bin/frp_version.txt)
-  LOG_INFO "FRP_VERSION : 七牛云 - $FRP_VERSION"
+  LOG_INFO "FRP_VERSION : 七牛云 - ${FRP_VERSION}"
 fi
 
 FILE_NAME="frp_${FRP_VERSION}_${KERNEL}_${MACHINE}.tar.gz"
---
+++ n2n_ntop/down_n2n.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+. init_logger.sh
+. init_kernel_name_and_machine_name.sh
+
+if [[ -z ${KERNEL} ]]; then
+    case ${myos} in
+    linux)
+        LOG_INFO "受支持的系统 - ${myos}"
+        KERNEL="linux"
+        ;;
+    macosx)
+        KERNEL="darwin"
+        LOG_INFO "受支持的系统 - ${myos}"
+        ;;
+    windows)
+        LOG_INFO "受支持的系统 - ${myos}"
+        KERNEL="windows"
+        ;;
+    *)
+        LOG_ERROR "不支持的系统 - ${myos}"
+        exit 1
+        ;;
+    esac
+
+fi
+if [[ -z ${MACHINE} ]]; then
+    case ${mycpu} in
+    i386)
+        LOG_INFO "受支持的CPU架构类型 - ${mycpu}"
+        MACHINE="x86"
+        ;;
+    amd64)
+        LOG_INFO "受支持的CPU架构类型 - ${mycpu}"
+        MACHINE="x64"
+        ;;
+    arm)
+        LOG_INFO "受支持的CPU架构类型 - ${mycpu}"
+        MACHINE="arm"
+        ;;
+    arm64)
+        LOG_INFO "受支持的CPU架构类型 - ${mycpu}"
+        MACHINE="arm64(aarch64)"
+        ;;
+    mips | mips64 | mips64el | mipsel | amd64)
+        LOG_INFO "受支持的CPU架构类型 - ${mycpu}"
+        MACHINE=$mycpu
+        ;;
+    *)
+        LOG_ERROR "不支持的CPU架构类型 - ${mycpu}"
+        exit 1
+        ;;
+    esac
+fi
+
+FILE_NAME=$(
+    curl -sS https://github.com/lucktu/n2n/tree/master/$(echo ${KERNEL} | sed -e 's/\b\(.\)/\u\1/g') |
+        grep -oP "(?<=\")n2n_v3_${KERNEL}_${MACHINE}.*?zip"
+)
+wget -O "/tmp/n2n.zip" "https://raw.githubusercontent.com/lucktu/n2n/master/$(echo ${KERNEL} | sed -e 's/\b\(.\)/\u\1/g')/${FILE_NAME}"
+unzip unzip -o -d "/tmp/n2n" "/tmp/n2n.zip"
+if [[ -d "/tmp/n2n/static" ]]; then
+    cp -r "/tmp/n2n/static/*" "/tmp/n2n/"
+fi
