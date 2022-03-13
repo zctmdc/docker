7cfca72117f5ed424f02615d1f11ffec12bd5f1f
ZCTMDC <zctmdc@outlook.com>
2022-03-13 16:54:07 +0800

    日志修复

--- n2n_ntop/down_n2n.sh
+++ n2n_ntop/down_n2n.sh
@@ -5,15 +5,12 @@
 if [[ -z ${KERNEL} ]]; then
     case ${myos} in
     linux)
-        LOG_INFO "受支持的系统 - ${myos}"
         KERNEL="linux"
         ;;
     macosx)
         KERNEL="darwin"
-        LOG_INFO "受支持的系统 - ${myos}"
         ;;
     windows)
-        LOG_INFO "受支持的系统 - ${myos}"
         KERNEL="windows"
         ;;
     *)
@@ -21,28 +18,25 @@ if [[ -z ${KERNEL} ]]; then
         exit 1
         ;;
     esac
-
+    LOG_INFO "受支持的系统 - ${myos} -> ${KERNEL}"
 fi
+
 if [[ -z ${MACHINE} ]]; then
     case ${mycpu} in
     i386)
-        LOG_INFO "受支持的CPU架构类型 - ${mycpu}"
         MACHINE="x86"
         ;;
     amd64)
-        LOG_INFO "受支持的CPU架构类型 - ${mycpu}"
         MACHINE="x64"
         ;;
     arm)
-        LOG_INFO "受支持的CPU架构类型 - ${mycpu}"
         MACHINE="arm"
         ;;
     arm64)
-        LOG_INFO "受支持的CPU架构类型 - ${mycpu}"
         MACHINE="arm64(aarch64)"
         ;;
     mips | mips64 | mips64el | mipsel | amd64)
-        LOG_INFO "受支持的CPU架构类型 - ${mycpu}"
+
         MACHINE=$mycpu
         ;;
     *)
@@ -50,30 +44,61 @@ if [[ -z ${MACHINE} ]]; then
         exit 1
         ;;
     esac
+    LOG_INFO "受支持的CPU架构类型 - ${mycpu} -> ${MACHINE}"
 fi
 
+VERSION="_$VERSION"
+case $(tr '[A-Z]' '[a-z]' <<<${VERSION##*_}) in
+v1)
+    SRC_DIR=n2n_v1
+    N2N_VERSION=v1
+    ;;
+v2)
+    SRC_DIR=n2n_v2
+    N2N_VERSION=v2
+    ;;
+v2s)
+    SRC_DIR=n2n_v2s
+    N2N_VERSION=v2s
+    ;;
+*)
+    SRC_DIR=""
+    N2N_VERSION=v3
+    ;;
+esac
+
+LOG_INFO "N2N_VERSION - ${VERSION#*_} -> ${SRC_DIR:+$SRC_DIR:}${N2N_VERSION}"
+
 FILE_NAME=$(
-    curl -k -sS https://github.com/lucktu/n2n/tree/master/$(echo ${KERNEL} | sed -e 's/\b\(.\)/\u\1/g') |
-        grep -oP "(?<=\")n2n_v3_${KERNEL}_$(echo $MACHINE | sed 's/(/\\(/' | sed 's/)/\\)/')_.*?zip"
+    curl -k -sS https://github.com/lucktu/n2n/tree/master/$(echo ${KERNEL} | sed -e 's/\b\(.\)/\u\1/g')${SRC_DIR:+/$SRC_DIR} |
+        grep -oP "(?<=\")n2n_${N2N_VERSION}_${KERNEL}_$(echo $MACHINE | sed 's/(/\\(/' | sed 's/)/\\)/')_.*?zip"
 )
 echo ${FILE_NAME}
+
 if [[ -z ${FILE_NAME} ]]; then
     LOG_ERROR "错误的文件名 - ${FILE_NAME}"
     LOG_ERROR "检查相关变量 - KERNEL:${KERNEL}, MACHINE:${MACHINE}"
-    exit 1
+    # exit 1
 fi
-wget --no-check-certificate -qO "/tmp/n2n.zip" "https://raw.githubusercontent.com/lucktu/n2n/master/$(echo ${KERNEL} | sed -e 's/\b\(.\)/\u\1/g')/${FILE_NAME}"
 
-unzip -o -d /tmp/n2n/ "/tmp/n2n.zip"
+wget --no-check-certificate -qO "/tmp/n2n.zip" "https://raw.githubusercontent.com/lucktu/n2n/master/$(echo ${KERNEL} | sed -e 's/\b\(.\)/\u\1/g')${SRC_DIR:+/$SRC_DIR}/${FILE_NAME}"
+
+unzip -o -d /tmp/n2n/ /tmp/n2n.zip 
+
+if ls /tmp/n2n/n2n*; then
+    mv /tmp/n2n/n2n*/* /tmp/n2n/
+    rm -r /tmp/n2n/n2n*/
+fi
+
 if [[ -d "/tmp/n2n/static" ]]; then
     cp -r /tmp/n2n/static/* /tmp/n2n/
 fi
 
 if [[ `ls /tmp/n2n/ | grep 'tar.gz'` ]] ; then
-    LOG_INFO "发现多版本"
+    LOG_INFO "发现多版本，即将解压"
     tar -zxvf $(ls /tmp/n2n/ | grep 'tar.gz' | grep '_zstd_' | grep '_static_') -C /tmp/n2n/ 
 fi
 
-
+chmod +x /tmp/n2n/*
 
 ls -l /tmp/n2n
