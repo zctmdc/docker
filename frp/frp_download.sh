68f6c72b968be0f3acd72b222a6f6059bf16e314
ZCTMDC <zctmdc@outlook.com>
2022-03-13 18:36:15 +0800

    优化日志输出，移除旧支持

--- frp/frp_download.sh
+++ frp/frp_download.sh
@@ -5,11 +5,9 @@
 if [[ -z ${KERNEL} ]]; then
   case ${myos} in
   linux | freebsd | windows)
-    LOG_INFO "受支持的系统 - ${myos}"
     KERNEL=${myos}
     ;;
   macosx)
-    LOG_INFO "受支持的系统 - ${myos}"
     KERNEL="darwin"
     ;;
   *)
@@ -17,16 +15,14 @@ if [[ -z ${KERNEL} ]]; then
     exit 1
     ;;
   esac
-
+  LOG_INFO "受支持的系统 - ${myos} -> ${KERNEL}"
 fi
 if [[ -z ${MACHINE} ]]; then
   case ${mycpu} in
   i386)
     MACHINE="386"
-    LOG_INFO "受支持的CPU架构类型 - ${mycpu}"
     ;;
   arm | arm64 | mips | mips64 | mips64el | mipsel | amd64)
-    LOG_INFO "受支持的CPU架构类型 - ${mycpu}"
     MACHINE=${mycpu}
     ;;
   *)
@@ -34,7 +30,7 @@ if [[ -z ${MACHINE} ]]; then
     exit 1
     ;;
   esac
-
+  LOG_INFO "受支持的CPU架构类型 - ${mycpu} -> ${MACHINE}"
 fi
 
 if [[ -z ${FRP_VERSION} ]]; then
@@ -47,11 +43,12 @@ if [[ -z ${FRP_VERSION} ]]; then
   LOG_INFO "FRP_VERSION : GITHUB - ${FRP_VERSION}"
 fi
 
-if [[ -z ${FRP_VERSION} ]]; then
-  LOG_INFO "正在从七牛云获取版本"
-  FRP_VERSION=$(curl -k -sS http://rt.qiniu.zctmdc.cn/bin/frp_version.txt)
-  LOG_INFO "FRP_VERSION : 七牛云 - ${FRP_VERSION}"
-fi
+### 下线
+# if [[ -z ${FRP_VERSION} ]]; then
+#   LOG_INFO "正在从七牛云获取版本"
+#   FRP_VERSION=$(curl -k -sS http://rt.qiniu.zctmdc.cn/bin/frp_version.txt)
+#   LOG_INFO "FRP_VERSION : 七牛云 - ${FRP_VERSION}"
+# fi
 
 FILE_NAME="frp_${FRP_VERSION}_${KERNEL}_${MACHINE}.tar.gz"
 wget --no-check-certificate -qO ${FILE_NAME} https://github.com/fatedier/frp/releases/download/v${FRP_VERSION}/${FILE_NAME}
