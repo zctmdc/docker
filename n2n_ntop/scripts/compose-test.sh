05ecedd1b09a16abcb9a9d2206d303e017be0889
zctmdc <zctmdc@outlook.com>
2022-11-17 06:02:24 +0800

    手动计时

--- n2n_ntop/scripts/compose-test.sh
+++ n2n_ntop/scripts/compose-test.sh
@@ -15,6 +15,9 @@ fi
 if [[ -z "${test_platform}" ]]; then
     test_platform="linux/amd64"
 fi
+
+TOTLA_WAIT_TIME=$((60 * 10))
+
 start() {
     docker compose --project-directory "${compsoe_file%/*}/" -f "${compsoe_file}" up -d
 }
@@ -22,7 +25,14 @@ stop() {
     docker compose -f "${compsoe_file}" down
 }
 check_status() {
+    startTime=$(date +%Y%m%d-%H:%M:%S)
+    startTime_s=$(date +%s)
+
     while true; do
+        nowTime=$(date +%Y%m%d-%H:%M:%S)
+        nowTime_s=$(date +%s)
+        sumTime=$(($nowTime_s - $startTime_s))
+
         run_status="$(docker compose -f ${compsoe_file} ps)"
         count_all_runing=$(echo "${run_status}" | grep test-n2n | wc -l)
         count_healthy=$(echo "${run_status}" | grep healthy | wc -l)
@@ -31,14 +41,18 @@ check_status() {
         count_created=$(echo "${run_status}" | grep created | wc -l)
 
         LOG_INFO "### run_status:\n${run_status}"
-
         LOG_INFO "count_healthy: ${count_healthy} / count_all_runing: ${count_all_runing}"
 
         if [[ "${count_all_runing}" == "${count_healthy}" ]]; then
-            LOG_INFO "已通过: ${count_healthy}/${count_all_runing}"
+            LOG_INFO "已通过: ${count_healthy}/${count_all_runing} - ${sumTime}s"
             return 0
         else
-            LOG_WARNING "测试中: ${count_healthy}/${count_all_runing}\n"
+            LOG_WARNING "测试中: ${count_healthy}/${count_all_runing} - ${sumTime}s\n"
+        fi
+
+        if [[ ${sumTime} -gt ${TOTLA_WAIT_TIME} ]]; then
+            LOG_ERROR "超时退出"
+            return 1
         fi
         sleep 10
     done
@@ -51,10 +65,11 @@ main() {
     start >>/dev/null 2>&1 &
     LOG_INFO "测试中"
     sleep 10
-    timeout $((60 * 10)) $0 check_status
+    check_status
     status_code=$?
-    LOG_INFO "测试结束 ${status_code}"
+    LOG_INFO "测试结果 ${status_code}"
     stop
+    LOG_INFO "测试结束"
     exit ${status_code}
 }
 
