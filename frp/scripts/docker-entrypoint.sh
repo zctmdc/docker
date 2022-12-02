d782284ba3b6ce2ac947d365224f5b4657afcd0a
ZCTMDC <zctmdc@outlook.com>
2022-12-03 06:08:29 +0800

    修复bug

--- frp/scripts/docker-entrypoint.sh
+++ frp/scripts/docker-entrypoint.sh
@@ -2,10 +2,10 @@
 
 MODE=${1:-$MODE}
 
-if [ "$MODE^^" = "RUN_FRPC" ];then
+if [ "${MODE^^}" = "RUN_FRPC" ];then
 
     /usr/bin/frpc -c /etc/frp/frpc.ini
-elif [ "$MODE^^" = "RUN_FRPS" ];then
+elif [ "${MODE^^}" = "RUN_FRPS" ];then
     /usr/bin/frps -c /etc/frp/frps.ini
 fi
 
