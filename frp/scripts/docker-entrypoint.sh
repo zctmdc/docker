3c1608b6a4ddc7c9cd13fe5df95862051c4743a7
ZCTMDC <zctmdc@outlook.com>
2022-12-03 12:43:27 +0800

    用不了？

--- frp/scripts/docker-entrypoint.sh
+++ frp/scripts/docker-entrypoint.sh
@@ -2,10 +2,10 @@
 
 MODE=${1:-$MODE}
 
-if [ "$(echo ${MODE} | tr a-z A-Z)" = "RUN_FRPC" ]; then
+if [ "$(echo ${MODE} | tr 'a-z' 'A-Z')" = "RUN_FRPC" ]; then
     exec /usr/bin/frpc -c /etc/frp/frpc.ini
-elif [ "$(echo ${MODE} | tr a-z A-Z)" = "RUN_FRPS" ]; then
+elif [ "$(echo ${MODE} | tr 'a-z' 'A-Z')" = "RUN_FRPS" ]; then
     exec /usr/bin/frps -c /etc/frp/frps.ini
 fi
 
-exec $@
+exec $*
