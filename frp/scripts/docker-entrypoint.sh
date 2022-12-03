6afe0cf0bfca9057a766dfd1e419fb58adefa308
ZCTMDC <zctmdc@outlook.com>
2022-12-03 12:30:47 +0800

    sh 里面有些东西不持支···

--- frp/scripts/docker-entrypoint.sh
+++ frp/scripts/docker-entrypoint.sh
@@ -2,11 +2,10 @@
 
 MODE=${1:-$MODE}
 
-if [ "${MODE^^}" = "RUN_FRPC" ];then
-
-    /usr/bin/frpc -c /etc/frp/frpc.ini
-elif [ "${MODE^^}" = "RUN_FRPS" ];then
-    /usr/bin/frps -c /etc/frp/frps.ini
+if [ "$(echo ${MODE} | tr a-z A-Z)" = "RUN_FRPC" ]; then
+    exec /usr/bin/frpc -c /etc/frp/frpc.ini
+elif [ "$(echo ${MODE} | tr a-z A-Z)" = "RUN_FRPS" ]; then
+    exec /usr/bin/frps -c /etc/frp/frps.ini
 fi
 
-exec "$@"
+exec $@
