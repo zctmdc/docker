8455425c2742250fbc24335c06221b660924990a
DT-AOUR <zctmdc@outlook.com>
2023-06-07 23:37:31 +0800

    rename py name

--- n2n/scripts/build/1x1_n2n-scan-all.sh
+++ n2n/scripts/build/1x1_n2n-scan-all.sh
@@ -15,8 +15,8 @@ LOG_INFO "activate .venv"
 source .venv/bin/activate
 LOG_INFO "install requirements"
 pip install -r requirements.txt
-LOG_INFO "run scan-all-n2n.py"
-python scan-all-n2n.py
+LOG_INFO "run 1x1_n2n-scan-all.py"
+python 1x1_n2n-scan-all.py
 LOG_INFO "echo to GITHUB_OUTPUT"
 
 ##########
