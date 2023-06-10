bd24ab777969ae714c022cd59c87b34086a8c0d5
EH-codeserver <zctmdc@outlook.com>
2023-06-10 09:26:57 +0800

    进一步测试下

--- n2n/scripts/build/2x5_n2n_sel_biggest.sh
+++ n2n/scripts/build/2x5_n2n_sel_biggest.sh
@@ -2,7 +2,7 @@
 
 source 0x0_init-logger.sh
 
-set -o errexit
+# set -o errexit
 set -o nounset
 # set -o pipefail
 
@@ -22,20 +22,22 @@ chmod +x ${n2n_edge_biggest}
 # source 3x0_n2n_fixlib.sh
 
 down_version="$(${n2n_edge_biggest} -h 2>&1 | grep Welcome | grep -Eo 'v\.?[0-9]\.[0-9]\.[0-9]' | grep -Eo '[0-9]\.[0-9]\.[0-9]')"
-
+LOG_ERROR "$(${n2n_edge_biggest} -h 2>&1)"
+# if [[ -z "${down_version}" ]]; then
+#     LOG_ERROR "$(${n2n_edge_biggest} -h 2>&1)"
+#     # exit 1
+# fi
 if [[ -n "${VERSION_B_S_rC}" ]]; then
     define_version="$(echo ${VERSION_B_S_rC} | grep -Eo '[0-9]\.[0-9]\.[0-9]')"
 else
     define_version="$(echo ${VERSION_SMALL} | grep -Eo '[0-9]\.[0-9]\.[0-9]')"
 fi
-if [[ -z "${down_version}"  ]]; then
+if [[ -z "${down_version}" ]]; then
     LOG_ERROR "需要修复: ${define_version}"
-    LOG_ERROR "$(${n2n_edge_biggest} -h  2>&1)"
     # exit 1
 fi
-if [[ "${define_version}" != "${down_version}"  ]]; then
+if [[ "${define_version}" != "${down_version}" ]]; then
     LOG_ERROR "下载版本不匹配: ${define_version} != ${down_version}"
-    LOG_ERROR "$(${n2n_edge_biggest} -h  2>&1)"
     # exit 1
 fi
 
