6a747675f877f8832654bba102af1e16dafdbc73
EH-codeserver <zctmdc@outlook.com>
2023-06-15 10:58:41 +0800

    打印 supernode 结果

--- n2n/scripts/build/test/docker-build-test.sh
+++ n2n/scripts/build/test/docker-build-test.sh
@@ -20,7 +20,6 @@ export DOCKER_BUILD_PLATFORMS="${DOCKER_BUILD_PLATFORMS}"
 export DOCKER_TEST_COMPSOE_FILE="${DOCKER_TEST_COMPSOE_FILE}"
 flag_test_pass='true'
 
-
 l_platforms=(${DOCKER_BUILD_PLATFORMS//,/ })
 for test_platform in ${l_platforms[@]}; do
     TEST_PLATFORM="${test_platform}"
@@ -39,13 +38,14 @@ for test_platform in ${l_platforms[@]}; do
         edge -h 2>&1 | xargs -0 --no-run-if-empty -I {} echo {})"
 
     LOG_WARNING "${edge_result}"
-    
+
     if [[ -z "$(echo ${edge_result,,} | grep welcome)" ]]; then
         LOG_ERROR 出错了: ${test_platform} - ${edge_result}
         flag_test_pass="false"
         continue
     fi
     LOG_WARNING "Test pass - edge"
+
     LOG_WARNING "Test start - supernode"
     docker run --rm \
         --platform ${test_platform} \
@@ -53,10 +53,12 @@ for test_platform in ${l_platforms[@]}; do
         ls /usr/local/sbin/supernode
 
     # supernode will not pass
-    edge_result="$(docker run --rm \
+    supernode_result="$(docker run --rm \
         --platform ${test_platform} \
         ${REGISTRY}/${REGISTRY_USERNAME}/${DOCKER_APP_NAME}:${DOCKER_TEST_TAG} \
         supernode -h 2>&1 | xargs -0 --no-run-if-empty -I {} echo {} || echo -e '\n# check supernode ignore errors')"
+        
+    LOG_WARNING "${supernode_result}"
 
     LOG_WARNING "Test done - supernode"
 done
